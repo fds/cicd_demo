a6fd6f100064b2dc05e94e13bee66c6f6bdc74e5
liss <1753900424@qq.com>
2019-10-17 09:56:40 +0800

    init

--- service-client/src/main/java/com/liss/serviceclient/client/HiClient.java
+++ service-client/src/main/java/com/liss/serviceclient/client/HiClient.java
@@ -1,8 +1,8 @@
 package com.liss.serviceclient.client;
 
 import com.liss.serviceclient.fallback.HiFallBack;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(value = "service-hi",fallback = HiFallBack.class)
 public interface HiClient {
